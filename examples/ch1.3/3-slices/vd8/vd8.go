91875c1f5f37556df456c540a0b5dc39a816c20e
ThoaiNguyen <thoaikhmt@gmail.com>
2019-06-07 22:23:23 +0700

    create examples

--- examples/ch1.3/3-slices/vd8/vd8.go
+++ examples/ch1.3/3-slices/vd8/vd8.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var a = []int{1, 2, 3}
+	var a = []int{1, 2, 3, 4, 5, 6}
 	N := 2
 	a = a[:len(a)-1] // xóa một phần tử ở cuối
 	a = a[:len(a)-N] // xóa N phần tử ở cuối
--- examples/ch1.3/3-slices/vd9/vd9.go
+++ examples/ch1.3/3-slices/vd9/vd9.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var a = []int{1, 2, 3}
+	var a = []int{1, 2, 3, 4, 5, 6}
 	N := 2
 	a = a[1:] // xóa phần tử đầu tiên
 	a = a[N:] // xóa N phần tử đầu tiên
