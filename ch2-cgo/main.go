254801c2ff0b91568d68f5bb4f42bf4e8ad65c77
Trung Duong Viet <dvt1996@yahoo.com.vn>
2019-06-10 07:01:21 +0700

    Update

--- ch2-cgo/main.go
+++ ch2-cgo/main.go
@@ -1,1 +1,19 @@
-package ch2_cgo
+package main
+
+//#include <stdio.h>
+import "C"
+import (
+	"fmt"
+	"time"
+)
+
+
+
+func main() {
+	C.puts(
+		C.CString("Hello, World\n"),
+	)
+	fmt.Println("Test")
+	time.Sleep(2 * time.Second)
+
+}
